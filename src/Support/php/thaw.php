25d279bea0dd0b03968e294b66d7be4c855e8346
Harald Lapp <harald@octris.org>
2014-11-05 22:45:00 +0100

    changes for new dialog class

--- src/Support/php/thaw.php
+++ src/Support/php/thaw.php
@@ -1,25 +1,32 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * This file is part of the 'octris/octris-tmbundle' package.
+ *
+ * (c) Harald Lapp <harald@octris.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * Thaw octris state for debugging purpose.
  *
- * @octdoc      php/thaw.php
- * @copyright   copyright © 2009-2013 by Harald Lapp
+ * @copyright   copyright (c) 2009-2014 by Harald Lapp
  * @author      Harald Lapp <harald@octris.org>
  */
-/**/
 
 $params = array();
 
-require_once('libs/tmdialog.class.php');
+require_once(__DIR__ . '/vendor/autoload.php');
 
-$dia = new tmdialog();
+$dia = new \Octris\TMDialog\Dialog();
 
 if ($dia->load('thaw', $params) === false) {
     // dialog was not loaded
     fputs(STDERR, "unable to load dialog 'thaw.nib'.\n");
-    
+
     exit(1);
 }
 
